8f41d089c72ced50588f8f4cd7b21b8ac9ecbfe4
Krzysztof Zaleski <kz@lertan.pl>
2025-06-30 07:55:52 +0200

    Added vscode

---
+++ vscode/extensions.sh
@@ -0,0 +1,10 @@
+code --install-extension eamodio.gitlens
+code --install-extension pkief.material-icon-theme
+code --install-extension ms-ceintl.vscode-language-pack-pl
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-python.python
+code --install-extension asciidoctor.asciidoctor-vscode
+code --install-extension njpwerner.autodocstring
+code --install-extension aaron-bond.better-comments
+code --install-extension samuelcolvin.jinjahtml
+code --install-extension wmaurer.change-case
